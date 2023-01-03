b5ff81e6ca00f0f8ce1245a26cebc64a50237432
fretchen <fred.jendrzejewski@gmail.com>
2023-01-03 16:24:48 +0100

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+# Allowing for secure communication on heroku
 ALLOWED_HOSTS = ["*"]
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 
 
 # Application definition
