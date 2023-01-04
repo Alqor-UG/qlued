b91d8d889f292aa8ec055a9561fd7a6775d61ad0
fretchen <fred.jendrzejewski@gmail.com>
2023-01-04 06:53:57 +0100

    Update tests.py

--- backends/tests.py
+++ backends/tests.py
@@ -347,11 +347,15 @@ class JobSubmissionTest(TestCase):
         data = json.loads(req.content)
         self.assertEqual(data["status"], "INITIALIZING")
         self.assertEqual(req.status_code, 200)
-        
-        #clean up the file
-        file_path = "/Backend_files/Queued_Jobs/fermions/job-{}.json".format(data['job_id'])
+
+        # clean up the file
+        file_path = "/Backend_files/Queued_Jobs/fermions/job-{}.json".format(
+            data["job_id"]
+        )
         self.storage_provider.delete_file(file_path)
-        file_path = "/Backend_files/Status/fermions/{}/status-{}.json".format(self.username , data['job_id'])
+        file_path = "/Backend_files/Status/fermions/{}/status-{}.json".format(
+            self.username, data["job_id"]
+        )
         self.storage_provider.delete_file(file_path)
 
     def test_get_job_status(self):
@@ -401,9 +405,13 @@ class JobSubmissionTest(TestCase):
         data = json.loads(req.content)
         self.assertEqual(data["job_id"], req_id)
 
-        #clean up the file
-        file_path = "/Backend_files/Queued_Jobs/fermions/job-{}.json".format(data['job_id'])
-        file_path = "/Backend_files/Status/fermions/{}/status-{}.json".format(self.username , data['job_id'])
+        # clean up the file
+        file_path = "/Backend_files/Queued_Jobs/fermions/job-{}.json".format(
+            data["job_id"]
+        )
+        file_path = "/Backend_files/Status/fermions/{}/status-{}.json".format(
+            self.username, data["job_id"]
+        )
 
     def test_get_next_job_in_queue(self):
         """
