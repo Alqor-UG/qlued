0e32f1f5ccd707dda03b03a3c410f3b81568f338
fretchen <fred.jendrzejewski@gmail.com>
2023-02-10 19:56:23 +0100

    Update admin.py

--- backends/admin.py
+++ backends/admin.py
@@ -4,7 +4,7 @@ Module that defines some basic properties of the app.
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Backend
 
 # Register your models here.
 
