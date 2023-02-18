735657df19dfe623d6ac0916189fa7139fd56c4d
fretchen <fred.jendrzejewski@gmail.com>
2023-02-18 23:16:42 +0100

    Moved the function (#31)
    Also added the appropiate separation of tests in there.

--- backends/tests.py
+++ backends/tests.py
@@ -6,7 +6,7 @@ import uuid
 
 from decouple import config
 from django.test import TestCase
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 from django.contrib.auth import get_user_model
 from .models import Backend
 from .apps import BackendsConfig as ac
@@ -256,40 +256,6 @@ class BackendConfigTest(TestCase):
             if gate["name"] == "fint":
                 self.assertEqual(gate["coupling_map"], [[0, 1, 2, 3, 4, 5, 6, 7]])
 
-    def test_fermions_get_config_ninja(self):
-        """
-        Test the API that presents the capabilities of the backend through the new version
-         of the API
-        """
-        url = reverse_lazy("api-1.0.0:get_config", kwargs={"backend_name": "fermions"})
-        req = self.client.get(
-            url, {"username": self.username, "password": self.password}
-        )
-        data = json.loads(req.content)
-        self.assertEqual(req.status_code, 200)
-        self.assertCountEqual(data["basis_gates"], ["fhop", "fint", "fphase"])
-        self.assertEqual(data["backend_name"], "alqor_fermionic_tweezer_simulator")
-        self.assertEqual(data["display_name"], "fermions")
-
-        base_url = config("BASE_URL")
-        self.assertEqual(data["url"], base_url + "/api/v1/fermions/")
-        self.assertEqual(data["n_qubits"], 8)
-        self.assertEqual(data["num_species"], 2)
-        gates = data["gates"]
-        for gate in gates:
-            if gate["name"] == "fhop":
-                self.assertEqual(
-                    gate["coupling_map"],
-                    [
-                        [0, 1, 2, 3],
-                        [2, 3, 4, 5],
-                        [4, 5, 6, 7],
-                        [0, 1, 2, 3, 4, 5, 6, 7],
-                    ],
-                )
-            if gate["name"] == "fint":
-                self.assertEqual(gate["coupling_map"], [[0, 1, 2, 3, 4, 5, 6, 7]])
-
     def test_singlequdit_get_config(self):
         """
         Test the API that presents the capabilities of the backend
@@ -359,84 +325,6 @@ class JobSubmissionTest(TestCase):
         user.set_password(self.password)
         user.save()
         self.storage_provider = getattr(ac, "storage")
-
-    def test_post_job(self):
-        """
-        Test the API that presents the capabilities of the backend
-        """
-        job_payload = {
-            "experiment_0": {
-                "instructions": [
-                    ("load", [7], []),
-                    ("load", [2], []),
-                    ("measure", [2], []),
-                    ("measure", [6], []),
-                    ("measure", [7], []),
-                ],
-                "num_wires": 8,
-                "shots": 4,
-                "wire_order": "sequential",
-            },
-        }
-
-        url = reverse("post_job", kwargs={"backend_name": "fermions"})
-        req = self.client.post(
-            url,
-            {
-                "json": json.dumps(job_payload),
-                "username": self.username,
-                "password": self.password,
-            },
-        )
-        data = json.loads(req.content)
-        self.assertEqual(data["status"], "INITIALIZING")
-        self.assertEqual(req.status_code, 200)
-
-        # clean up the file
-        file_path = f"/Backend_files/Queued_Jobs/fermions/job-{data['job_id']}.json"
-        self.storage_provider.delete_file(file_path)
-        file_path = f"/Backend_files/Status/fermions/{self.username}/status-{data['job_id']}.json"
-        self.storage_provider.delete_file(file_path)
-
-    def test_post_job_ninja(self):
-        """
-        Test the API that presents the capabilities of the backend
-        """
-        job_payload = {
-            "experiment_0": {
-                "instructions": [
-                    ("load", [7], []),
-                    ("load", [2], []),
-                    ("measure", [2], []),
-                    ("measure", [6], []),
-                    ("measure", [7], []),
-                ],
-                "num_wires": 8,
-                "shots": 4,
-                "wire_order": "sequential",
-            },
-        }
-
-        url = reverse_lazy("api-1.0.0:post_job", kwargs={"backend_name": "fermions"})
-
-        req = self.client.post(
-            url,
-            {
-                "job": json.dumps(job_payload),
-                "username": self.username,
-                "password": self.password,
-            },
-            content_type="application/json",
-        )
-        data = json.loads(req.content)
-        self.assertEqual(data["status"], "INITIALIZING")
-        self.assertEqual(req.status_code, 200)
-
-        # clean up the file
-        file_path = f"/Backend_files/Queued_Jobs/fermions/job-{data['job_id']}.json"
-        self.storage_provider.delete_file(file_path)
-        file_path = f"/Backend_files/Status/fermions/{self.username}/status-{data['job_id']}.json"
-        self.storage_provider.delete_file(file_path)
 
     def test_get_job_status(self):
         """
