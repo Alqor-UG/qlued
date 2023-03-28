ee79ce01f6d44749b639f4bf9e1000c3e9665e0c
fretchen <fred.jendrzejewski@gmail.com>
2023-03-28 20:49:01 +0300

    Make the login much smoother.

--- main/settings.py
+++ main/settings.py
@@ -75,7 +75,7 @@ AUTH_USER_MODEL = "backends.User"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
