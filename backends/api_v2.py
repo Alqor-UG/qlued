2537251347919733120269219753224acda21eef
fretchen <fred.jendrzejewski@gmail.com>
2023-07-16 21:18:42 +0200

    152 use the display name to identify the backend (#158)
    * Start tests and documentation
    
    * First simple test
    
    * Clean up the tests
    
    * Add tests and close everything up
    
    * Update api_v2.py

--- backends/api_v2.py
+++ backends/api_v2.py
@@ -17,22 +17,46 @@ from .schemas import (
 from .apps import BackendsConfig as ac
 from .models import Token
 
+from .storage_providers import get_short_backend_name
+
+
 api = NinjaAPI(version="2.0.0")
 
 
 @api.get(
     "{backend_name}/get_config",
-    response=BackendSchemaOut,
+    response={200: BackendSchemaOut, codes_4xx: JobResponseSchema},
     tags=["Backend"],
     url_name="get_config",
 )
 def get_config(request, backend_name: str):
     """
     Returns the list of backends.
+
+    Args:
+        request: The request object.
+        backend_name: The name of the backend.
+
+    Raises:
+        404: If the backend is not found.
     """
     # pylint: disable=W0613
+
+    # we have to split the name into several parts by `_`. If there is only one part, then we
+    # assume that the user has given the short name of the backend. If there are more parts, then
+    # we assume that the user has given the full name of the backend.
+    short_backend = get_short_backend_name(backend_name)
+    if not short_backend:
+        job_response_dict = {
+            "job_id": "None",
+            "status": "ERROR",
+            "detail": "Unknown back-end! The string should have 1 or three parts separated by `_`!",
+            "error_message": "Unknown back-end!",
+        }
+        return 404, job_response_dict
+
     storage_provider = getattr(ac, "storage")
-    return storage_provider.get_backend_dict(backend_name)
+    return storage_provider.get_backend_dict(short_backend)
 
 
 @api.post(
@@ -65,10 +89,12 @@ def post_job(request, data: JobSchemaWithTokenIn, backend_name: str):
         return 401, job_response_dict
 
     username = token.user.username
+    # get the proper backend name
+    short_backend = get_short_backend_name(backend_name)
     # now it is time to look for the backend
     storage_provider = getattr(ac, "storage")
     backend_names = storage_provider.get_backends()
-    if not backend_name in backend_names:
+    if short_backend not in backend_names:
         job_response_dict["status"] = "ERROR"
         job_response_dict["detail"] = "Unknown back-end!"
         job_response_dict["error_message"] = "Unknown back-end!"
@@ -89,13 +115,13 @@ def post_job(request, data: JobSchemaWithTokenIn, backend_name: str):
 
         # upload the job to the backend via the storage provider
         job_id = storage_provider.upload_job(
-            job_dict=job_dict, backend_name=backend_name, username=username
+            job_dict=job_dict, backend_name=short_backend, username=username
         )
 
         # now we upload the status json to the backend. this is the same status json
         # that is returned to the user
         job_response_dict = storage_provider.upload_status(
-            backend_name=backend_name,
+            backend_name=short_backend,
             username=username,
             job_id=job_id,
         )
@@ -136,7 +162,8 @@ def get_job_status(request, backend_name: str, job_id: str, token: str):
     username = token_object.user.username
     storage_provider = getattr(ac, "storage")
     backend_names = storage_provider.get_backends()
-    if not backend_name in backend_names:
+    short_backend = get_short_backend_name(backend_name)
+    if short_backend not in backend_names:
         job_response_dict["status"] = "ERROR"
         job_response_dict["detail"] = "Unknown back-end!"
         job_response_dict["error_message"] = "Unknown back-end!"
@@ -159,7 +186,7 @@ def get_job_status(request, backend_name: str, job_id: str, token: str):
         storage_provider = getattr(ac, "storage")
 
         job_response_dict = storage_provider.get_status(
-            backend_name=backend_name, username=username, job_id=job_id
+            backend_name=short_backend, username=username, job_id=job_id
         )
 
         return 200, job_response_dict
@@ -201,10 +228,10 @@ def get_job_result(request, backend_name: str, job_id: str, token: str):
         return 401, status_msg_dict
 
     username = token_object.user.username
-
+    short_backend = get_short_backend_name(backend_name)
     storage_provider = getattr(ac, "storage")
     backend_names = storage_provider.get_backends()
-    if not backend_name in backend_names:
+    if short_backend not in backend_names:
         status_msg_dict["status"] = "ERROR"
         status_msg_dict["detail"] = "Unknown back-end!"
         status_msg_dict["error_message"] = "Unknown back-end!"
@@ -224,7 +251,7 @@ def get_job_result(request, backend_name: str, job_id: str, token: str):
     # request the data from the queue
     try:
         status_msg_dict = storage_provider.get_status(
-            backend_name=backend_name, username=username, job_id=job_id
+            backend_name=short_backend, username=username, job_id=job_id
         )
         if status_msg_dict["status"] != "DONE":
             return 200, status_msg_dict
@@ -239,7 +266,7 @@ def get_job_result(request, backend_name: str, job_id: str, token: str):
     # and if the status is switched to done, we can also obtain the result
     try:
         result_dict = storage_provider.get_result(
-            backend_name=backend_name, username=username, job_id=job_id
+            backend_name=short_backend, username=username, job_id=job_id
         )
 
         return 200, result_dict
